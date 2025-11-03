887dc04e634218719787caec6c88b19859454391
Dorazar <dorazar01@gmail.com>
2025-11-03 15:13:29 +0200

    Add code example

--- codeExample.js
+++ codeExample.js
@@ -16,30 +16,32 @@ async function getNewAlbumsReleases(limit = 21, offset = 0) {
     const endpoint = `/browse/new-releases?limit=${limit}&offset=${offset}&country=US`
     const response = await makeSpotifyRequest(endpoint)
     // Map albums to clean object format
-    const albums = response.albums.items.map((album) => ({
-      id: album.id,
-      name: album.name,
-      artists: album.artists.map((artist) => ({
-        id: artist.id,
-        name: artist.name,
-      })),
-      releaseDate: album.release_date,
-      totalTracks: album.total_tracks,
-      images: album.images.map((img) => ({
-        url: img.url,
-        height: img.height,
-        width: img.width,
-      })),
-      externalUrls: album.external_urls,
-      uri: album.uri,
-      albumType: album.album_type,
-    }))
 
-    return {
-      albums,
-    }
+    const albums = await Promise.all(response.albums.items.map(async (album) => await _organizeAlbumData(album)))
+    return albums
   } catch (error) {
-    console.error('Error fetching new album releases:', error)
     throw error
   }
+}
+
+async function _organizeAlbumData(album) {
+  const albumToShow = {
+    id: album.id,
+    name: album.name,
+    artists: album.artists.map((artist) => ({
+      id: artist.id,
+      name: artist.name,
+    })),
+    releaseDate: album.release_date,
+    totalTracks: album.total_tracks,
+    images: album.images.map((img) => ({
+      url: img.url,
+      height: img.height,
+      width: img.width,
+    })),
+    externalUrls: album.external_urls,
+    uri: album.uri,
+    albumType: album.album_type,
+  }
+  return albumToShow
 }
