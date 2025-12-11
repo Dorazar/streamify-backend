7413e3daf1c09bec608fa655805fe099efedbbaf
Dorazar <dorazar01@gmail.com>
2025-12-11 15:23:11 +0200

    Fix login page

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ if (process.env.NODE_ENV === 'production') {
             'http://localhost:3000',
             'http://127.0.0.1:5173',
             'http://localhost:5173',
-             'http://localhost:5174'
+             'http://localhost:5174',
+             'http://10.100.102.25:5174',
+             'http://localhost:4173'
         ],
         credentials: true
     }
